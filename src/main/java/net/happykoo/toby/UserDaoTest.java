31017cb209496e623b137faece8e1fdabfa1cd7d
kookyungmin <rudals4549@naver.com>
2024-07-13 00:25:54 +0900

    feat: spring 적용

--- src/main/java/net/happykoo/toby/UserDaoTest.java
+++ src/main/java/net/happykoo/toby/UserDaoTest.java
@@ -4,14 +4,16 @@ import net.happykoo.toby.dao.DaoFactory;
 import net.happykoo.toby.dao.MysqlConnectionMaker;
 import net.happykoo.toby.dao.UserDao;
 import net.happykoo.toby.dto.User;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 import java.util.Optional;
 
 public class UserDaoTest {
     public static void main(String[] args) {
         try {
-            UserDao userDao = new DaoFactory().userDao();
-
+            ApplicationContext context = new AnnotationConfigApplicationContext(DaoFactory.class);
+            UserDao userDao = context.getBean("userDao", UserDao.class);
             // id 가 1인 유저 조회
             User user = userDao.findById(1);
             String nickName = Optional.ofNullable(user)
--- src/main/java/net/happykoo/toby/dao/DaoFactory.java
+++ src/main/java/net/happykoo/toby/dao/DaoFactory.java
@@ -1,10 +1,16 @@
 package net.happykoo.toby.dao;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
 public class DaoFactory {
+    @Bean
     public UserDao userDao() {
         return new UserDao(connectionMaker());
     }
 
+    @Bean
     public ConnectionMaker connectionMaker() {
         return new MysqlConnectionMaker();
     }
