bcacab8fa22b6bb5698fcbfcb35bcfcec7547ba5
kookyungmin <rudals4549@naver.com>
2024-07-17 23:12:14 +0900

    feat: 싱글톤 테스트

--- src/main/java/net/happykoo/toby/UserDaoTest.java
+++ src/main/java/net/happykoo/toby/UserDaoTest.java
@@ -12,18 +12,34 @@ import java.util.Optional;
 public class UserDaoTest {
     public static void main(String[] args) {
         try {
-            ApplicationContext context = new AnnotationConfigApplicationContext(DaoFactory.class);
-            UserDao userDao = context.getBean("userDao", UserDao.class);
-            // id 가 1인 유저 조회
-            User user = userDao.findById(1);
-            String nickName = Optional.ofNullable(user)
-                    .map(User::getNickName)
-                    .orElse(null);
-            System.out.println("user nickName >> " + nickName);
+            //객체 직접 생성
+            DaoFactory daoFactory = new DaoFactory();
+            UserDao userDao1 = daoFactory.userDao();
+            UserDao userDao2 = daoFactory.userDao();
 
-            // 전체 유저 조회
-            int totalCount = userDao.findAll().size();
-            System.out.println("user all count >> " + totalCount);
+            System.out.println(userDao1.equals(userDao2));
+
+            //ApplicationContext 사용
+            ApplicationContext context = new AnnotationConfigApplicationContext(DaoFactory.class);
+            UserDao userDao3 = context.getBean("userDao", UserDao.class);
+            UserDao userDao4 = context.getBean("userDao", UserDao.class);
+
+            System.out.println(userDao3.equals(userDao4));
+
+
+
+
+
+//            // id 가 1인 유저 조회
+//            User user = userDao.findById(1);
+//            String nickName = Optional.ofNullable(user)
+//                    .map(User::getNickName)
+//                    .orElse(null);
+//            System.out.println("user nickName >> " + nickName);
+//
+//            // 전체 유저 조회
+//            int totalCount = userDao.findAll().size();
+//            System.out.println("user all count >> " + totalCount);
         } catch (Exception e) {
             e.printStackTrace();
         }
